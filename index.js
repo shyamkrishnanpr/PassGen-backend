e7b87d80299246c1ebb565a56106bd4a34f3ab93
shyam krishnan <shyamkrishnasby@gmail.com>
2023-10-17 20:53:22 +0530

    url updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ connectDatabase();
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:8000"],
+    origin: ["http://localhost:8000","https://safekey.netlify.app/"],
   })
 );
 
